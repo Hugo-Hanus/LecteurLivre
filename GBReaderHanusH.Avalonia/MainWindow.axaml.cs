07af616faef3943f7c6651835b527212909f3163
E190533 <h.hanus@student.helmo.be>
2022-10-05 11:18:10 +0200

    Initial commit

---
+++ GBReaderHanusH.Avalonia/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GBReaderHanusH.Avalonia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
